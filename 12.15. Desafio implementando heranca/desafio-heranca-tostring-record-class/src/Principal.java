3e475cead9da2c712a0852e7932cb524f7c728af
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2022-11-24 15:25:20 -0300

    codigo-fonte da aula

--- 12.15. Desafio implementando heranca/desafio-heranca-tostring-record-class/src/Principal.java
+++ 12.15. Desafio implementando heranca/desafio-heranca-tostring-record-class/src/Principal.java
@@ -6,12 +6,12 @@ public class Principal {
 
     public static void main(String[] args) {
 
-//        Funcionario funcionario1 = new Funcionario("Bruno", 40.0);
-//        System.out.println(funcionario1);
-//        Holerite holerite = funcionario1.gerarHolerite(10, "Dezembro");
-//        holerite.imprimir();
-//
-//        System.out.println();
+        Funcionario funcionario1 = new Funcionario("Bruno", 40.0);
+        System.out.println(funcionario1);
+        Holerite holerite = funcionario1.gerarHolerite(10, "Dezembro");
+        holerite.imprimir();
+
+        System.out.println();
 
         Programador programador1 = new Programador("Jeferson", 25.0);
         programador1.setValorBonus(100);
