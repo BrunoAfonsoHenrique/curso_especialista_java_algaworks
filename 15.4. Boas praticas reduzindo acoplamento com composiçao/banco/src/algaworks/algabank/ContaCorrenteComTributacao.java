ffdc5f659962343054aeec0dbd0eb5e3bfc45b60
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-01-19 21:00:33 -0300

    codigo-fonte da aula

---
+++ 15.4. Boas praticas reduzindo acoplamento com composiçao/banco/src/algaworks/algabank/ContaCorrenteComTributacao.java
@@ -0,0 +1,25 @@
+package algaworks.algabank;
+
+import com.algaworks.javabank.ContaCorrente;
+
+public class ContaCorrenteComTributacao extends ContaCorrente {
+
+    public static final double TAXA_IMPOSTO_MOVIMENTACAO = 0.1;
+
+    @Override
+    public void sacar(double valor) {
+        super.sacar(valor);
+        super.sacar(valor * TAXA_IMPOSTO_MOVIMENTACAO);
+
+    }
+
+    @Override
+    public void aplicarEmInvestimento(double valor) {
+        super.aplicarEmInvestimento(valor);
+        super.sacarTarifa(valor);
+    }
+    
+    public void sacarTarifa(double valorMovimentacao) {
+        super.sacar(valorMovimentacao * TAXA_IMPOSTO_MOVIMENTACAO);
+    }
+}
