714842c6cbc822ca59d997a3ffac07356dc5c4d3
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-01-19 21:23:28 -0300

    codigo-fonte da aula

--- 15.4. Boas praticas reduzindo acoplamento com composiçao/banco/src/algaworks/algabank/Principal.java
+++ 15.4. Boas praticas reduzindo acoplamento com composiçao/banco/src/algaworks/algabank/Principal.java
@@ -1,18 +1,17 @@
 package algaworks.algabank;
 
-import com.algaworks.javabank.Conta;
-import com.algaworks.javabank.ContaCorrente;
+import algaworks.javabank.Conta;
+import algaworks.javabank.ContaCorrente;
 
 public class Principal {
 
     public static void main(String[] args) {
-        Conta conta1 = new ContaCorrenteComTributacao();
+        Conta conta1 = new ContaComTributacao(new ContaCorrente());
         Conta conta2 = new ContaCorrente();
 
         conta1.depositar(1000);
- //       conta1.sacar(100);
+        conta1.sacar(100);
         conta1.transferir(conta2, 100);
-//        conta1.aplicarEmInvestimento(100);
 
         System.out.printf("Saldo da conta 1: R$%.2f%n", conta1.getSaldo());
         System.out.printf("Saldo da conta 2: R$%.2f%n", conta2.getSaldo());
