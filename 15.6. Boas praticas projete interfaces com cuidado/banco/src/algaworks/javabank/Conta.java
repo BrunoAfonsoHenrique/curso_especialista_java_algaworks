3eb68bb498d757fb8dcb5433d052973cae1b8557
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-01-20 20:58:08 -0300

    codigo-fonte da aula

--- 15.6. Boas praticas projete interfaces com cuidado/banco/src/algaworks/javabank/Conta.java
+++ 15.6. Boas praticas projete interfaces com cuidado/banco/src/algaworks/javabank/Conta.java
@@ -7,6 +7,9 @@ public interface Conta {
     void depositar(double valor);
     void transferir(Conta conta, double valor);
     void aplicarEmInvestimento(double valor);
+    default void imprimirSaldo() {
+        System.out.printf("Seu daldo é de: R$%.2f", getSaldo());
+    }
 
 
 }
