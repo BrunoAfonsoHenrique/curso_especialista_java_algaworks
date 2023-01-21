0c86600a261a3a3a3695695bb036e3008ae28d3a
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-01-20 21:18:19 -0300

    codigo-fonte da aula

--- 15.7. Boas praticas use interfaces apenas para definir tipos/calculadora-tempo/src/com/algaworks/tempo/CalculadoraTempo.java
+++ 15.7. Boas praticas use interfaces apenas para definir tipos/calculadora-tempo/src/com/algaworks/tempo/CalculadoraTempo.java
@@ -1,4 +1,16 @@
 package com.algaworks.tempo;
 
-public class CalculadoraTempo {
+public class CalculadoraTempo  {
+
+    public static final int SEGUNDOS_POR_MINUTO = 60;
+    public static final int SEGUNDOS_POR_HORA = 3600;
+
+    public static double converterSegundosEmHoras(int segundos) {
+        return (double) segundos / SEGUNDOS_POR_HORA;
+    }
+
+    public static double converterSegundosEmMinutos(int segundos) {
+        return (double) segundos / SEGUNDOS_POR_MINUTO;
+    }
+
 }
--- 15.7. Boas praticas use interfaces apenas para definir tipos/calculadora-tempo/src/com/algaworks/tempo/TempoConstants.java
+++ 15.7. Boas praticas use interfaces apenas para definir tipos/calculadora-tempo/src/com/algaworks/tempo/TempoConstants.java
@@ -1,4 +1,8 @@
 package com.algaworks.tempo;
 
 public interface TempoConstants {
+
+    public static final int SEGUNDOS_POR_MINUTO = 60;
+
+    public static final int SEGUNDOS_POR_HORA = 3600;
 }
