de158ee82aa3af250a0290ff87f7aafac399cf41
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-01-23 20:37:32 -0300

    codigo-fonte da aula

---
+++ 16.1. Introdução às exceções/excecoes/src/Principal1.java
@@ -0,0 +1,10 @@
+public class Principal1 {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio");
+
+        int x = 10 / 0;
+
+        System.out.println("Fim");
+    }
+}
