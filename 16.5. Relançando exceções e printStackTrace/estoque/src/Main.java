43994fd30370e4dca838e4d930ac460ea8e41960
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-01-24 20:31:32 -0300

    codigo-fonte da aula

--- 16.5. Relançando exceções e printStackTrace/estoque/src/Main.java
+++ 16.5. Relançando exceções e printStackTrace/estoque/src/Main.java
@@ -13,24 +13,35 @@ public class Main {
 
     public static void comprar(Produto produto) {
         Scanner sc = new Scanner(System.in);
-        System.out.printf("Quantidade: ");
-        int quantidade = sc.nextInt();
 
-        efetuarBaixaEstoque(produto, quantidade);
-        System.out.printf("Compra realizada");
+        do {
+            try {
+                System.out.printf("Quantidade: ");
+                int quantidade = sc.nextInt();
+
+                efetuarBaixaEstoque(produto, quantidade);
+                System.out.printf("Compra realizada");
+
+                break;
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+                System.out.println("Erro: " + e.getMessage());
+            }
+        } while(true);
 
     }
 
     public static void efetuarBaixaEstoque(Produto produto, int quantidade) {
-        try {
+  //      try {
             produto.retirarEstoque(quantidade);
             System.out.printf("%d unidades retiradas do estoque. Estoque atual %d%n ",
                     quantidade, produto.getQuantidadeEstoque());
         }
-        catch (IllegalArgumentException e) { // e -> nome da variavel que vai referenciar o objeto da excecao
-            // instruções para tratamento adequado da excecao
-            System.out.println("Erro ao efetuar baixa no estoque: " + e.getMessage());
-        }
+//        catch (IllegalArgumentException e) { // e -> nome da variavel que vai referenciar o objeto da excecao
+//            // instruções para tratamento adequado da excecao
+//            System.out.println("Erro ao efetuar baixa no estoque: " + e.getMessage());
+//            throw e;
+//        }
 
-    }
+//    }
 }
