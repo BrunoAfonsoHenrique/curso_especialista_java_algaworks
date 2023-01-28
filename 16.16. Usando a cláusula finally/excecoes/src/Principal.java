5fe3f285849ef38eacafe0cdd6208f2e24c72585
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-01-28 09:44:20 -0300

    codigo-fonte da aula

--- 16.16. Usando a cláusula finally/excecoes/src/Principal.java
+++ 16.16. Usando a cláusula finally/excecoes/src/Principal.java
@@ -7,14 +7,21 @@ public class Principal {
     public static void main(String[] args) {
 
         Path arquivo = Path.of("C:\\Users\\Bruno\\Documents\\especialista-java-algaWorks\\projetos-java\\especialista-java-algaworks\\16.16. Usando a cláusula finally\\excecoes\\src\\arquivo");
-
+        BufferedReader reader = null;
         try {
-            BufferedReader reader = Files.newBufferedReader(arquivo);
+            reader = Files.newBufferedReader(arquivo);
             System.out.println(reader.readLine());
 
             reader.close();
+
         } catch (IOException e) {
             System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                System.out.println("Erro fechando o leitor de arquivo");
+            }
         }
     }
 }
