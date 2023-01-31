5ae471c8358effe86cdd7eef14f335bb06df2032
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-01-31 10:03:28 -0300

    codigo-fonte da aula

---
+++ 17.1. Introdução aos Generics/generics/src/Principal.java
@@ -0,0 +1,26 @@
+
+
+import com.algaworks.crm.Cliente;
+
+import java.util.ArrayList;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        clientes.add(new Cliente("Supermercado Pague e Leve", 2_000_000));
+        clientes.add(new Cliente("Posto Gasolina Boa", 800_000));
+//        clientes.add("José");
+
+        double totalFaturamento = 0d;
+
+        for (Cliente cliente : clientes) {
+            //Cliente cliente = (Cliente) object;
+            totalFaturamento += cliente.getFaturamentoMensal();
+        }
+
+        System.out.println(totalFaturamento);
+
+
+    }
+}
