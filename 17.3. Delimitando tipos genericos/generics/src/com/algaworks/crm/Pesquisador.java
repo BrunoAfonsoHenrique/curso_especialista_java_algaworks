4d445b4dcd9228d9dc810f2cb0e81d0712224cae
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-04-13 20:37:10 -0300

    codigo-fonte da aula

--- 17.3. Delimitando tipos genericos/generics/src/com/algaworks/crm/Pesquisador.java
+++ 17.3. Delimitando tipos genericos/generics/src/com/algaworks/crm/Pesquisador.java
@@ -2,7 +2,8 @@ package com.algaworks.crm;
 
 public class Pesquisador {
 
-    public static <T extends Nomeavel> T obterPorNome(T[] item, String nome) { // Extends aqui não é herança
+//    public static <T extends Nomeavel && PessoaJuridica> T obterPorNome(T[] item, String nome) { // Extends aqui não é herança
+    public static <T extends Nomeavel> T obterPorNome(T[] item, String nome) {
         for (T funcionario : item) {
             if (funcionario.getNome().equals(nome)) {
                 return funcionario;
