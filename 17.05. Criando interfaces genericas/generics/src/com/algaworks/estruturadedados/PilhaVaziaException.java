77628aa1a969649849ce79edaa8226a88ad236a0
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-04-17 20:32:41 -0300

    codigo-fonte da aula

---
+++ 17.05. Criando interfaces genericas/generics/src/com/algaworks/estruturadedados/PilhaVaziaException.java
@@ -0,0 +1,8 @@
+package com.algaworks.estruturadedados;
+
+public class PilhaVaziaException extends RuntimeException {
+
+    public PilhaVaziaException(String message) {
+        super(message);
+    }
+}
