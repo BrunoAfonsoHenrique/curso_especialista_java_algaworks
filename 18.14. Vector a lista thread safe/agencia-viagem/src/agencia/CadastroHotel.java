248743c754177ca31adbf2cc2d95b1ea8d587a5b
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-06-13 11:27:59 -0300

    codigo-fonte da aula

---
+++ 18.14. Vector a lista thread safe/agencia-viagem/src/agencia/CadastroHotel.java
@@ -0,0 +1,53 @@
+package agencia;
+
+import agencia.exception.HotelJaExistenteException;
+import agencia.exception.HotelNaoEncontradoException;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class CadastroHotel {
+
+    private final Vector<Hotel> hoteis = new Vector<>();
+
+    public void adicionar(String nome, String cidade, double precoDiaria) {
+        Hotel hotel = new Hotel(nome, cidade, precoDiaria);
+
+        if (hoteis.contains(hotel)) {
+            throw new HotelJaExistenteException("Hotel já foi adicionado");
+        }
+
+//        hoteis.add(0, hotel);
+        hoteis.add(hotel);
+    }
+
+    public Vector<Hotel> obterTodos() {
+        return hoteis;
+    }
+
+
+    public void removerPorCidade(String cidade) {
+        Iterator<Hotel> hotelIterator = hoteis.iterator();
+        while (hotelIterator.hasNext()) {
+            Hotel hotel = hotelIterator.next();
+            if (hotel.getCidade().equals(cidade)) {
+                hotelIterator.remove(); // remove o ultimo elemento que o hotelIterator.next() retornou
+            }
+
+        }
+    }
+
+    public void remover(Hotel hotel) {
+        boolean removido = hoteis.remove(hotel);
+
+        if (!removido) {
+            throw new HotelNaoEncontradoException("Hotel nao encontrado para remocao");
+        }
+    }
+
+    public void removerTodos() {
+        hoteis.clear();
+    }
+
+}
---
+++ 18.14. Vector a lista thread safe/agencia-viagem/src/agencia/exception/HotelNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package agencia.exception;
+
+public class HotelNaoEncontradoException extends RuntimeException{
+
+    public HotelNaoEncontradoException(String message) {
+        super(message);
+    }
+}
