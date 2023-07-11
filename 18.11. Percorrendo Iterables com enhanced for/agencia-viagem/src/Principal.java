30d5d0811021eb8c63eb0bfb0a987f6b7c65c653
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-07-11 18:19:10 -0300

    codigo-fonte da aula

---
+++ 18.11. Percorrendo Iterables com enhanced for/agencia-viagem/src/Principal.java
@@ -0,0 +1,32 @@
+import com.algaworks.agencia.CadastroHotel;
+import com.algaworks.agencia.Hotel;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        CadastroHotel cadastro = new CadastroHotel();
+        cadastro.adicionarNovoHotel("Jaguaribe Lodge", "Fortim/CE", 1300);
+        cadastro.adicionarNovoHotel("Vila Selvagem", "Fortim/CE", 1400);
+        cadastro.adicionarNovoHotel("Hotel Fazenda Dona Carolina", "Itatiba/SP", 2200);
+        cadastro.adicionarNovoHotel("Tivoli Ecoresort", "Praia do Forte/BA", 2000);
+        cadastro.adicionarNovoHotel("Mercure", "Uberlândia/MG", 400);
+
+        ArrayList<Hotel> hoteis = cadastro.obterTodos();
+        imprimirHoteis(hoteis);
+    }
+
+
+    private static void imprimirHoteis(ArrayList<Hotel> hoteis) {
+
+        for (Hotel hotel : hoteis) {
+            System.out.printf("%s (%s) -> %.2f%n", hotel.getNome(),
+                    hotel.getCidade(), hotel.getPrecoDiaria());
+        }
+        
+    }
+}
