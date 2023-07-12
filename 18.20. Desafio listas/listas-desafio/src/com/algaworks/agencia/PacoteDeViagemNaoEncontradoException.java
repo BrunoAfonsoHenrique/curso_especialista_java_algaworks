020eb769e33999f0cdd47e9b95ea802f152dc392
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-07-12 13:18:56 -0300

    codigo-fonte da aula

---
+++ 18.20. Desafio listas/listas-desafio/src/com/algaworks/agencia/PacoteDeViagemNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.algaworks.agencia;
+
+public class PacoteDeViagemNaoEncontradoException extends RuntimeException{
+
+    public PacoteDeViagemNaoEncontradoException(String message) {
+        super(message);
+    }
+}
