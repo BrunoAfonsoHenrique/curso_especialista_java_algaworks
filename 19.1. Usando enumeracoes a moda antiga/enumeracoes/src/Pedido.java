fea5df500acf42aac3fc9d2db901992916785f08
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-07-25 15:02:25 -0300

    codigo-fonte da aula

---
+++ 19.1. Usando enumeracoes a moda antiga/enumeracoes/src/Pedido.java
@@ -0,0 +1,12 @@
+public class Pedido {
+
+    private String nomeCliente;
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+}
