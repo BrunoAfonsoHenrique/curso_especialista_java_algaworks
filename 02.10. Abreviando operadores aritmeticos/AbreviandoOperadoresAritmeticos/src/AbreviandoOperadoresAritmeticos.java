96422b83c2e3c501bf5c31c5cd2a41076889eee1
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-07-27 15:56:08 -0300

    codigo-fonte da aula

---
+++ 02.10. Abreviando operadores aritmeticos/AbreviandoOperadoresAritmeticos/src/AbreviandoOperadoresAritmeticos.java
@@ -0,0 +1,14 @@
+public class AbreviandoOperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        int quantidadeProdutos = 10;
+
+        // quantidadeProdutos += 3;
+        //quantidadeProdutos -= 3;
+        //quantidadeProdutos *= 3;
+        //quantidadeProdutos /= 3;
+        quantidadeProdutos %= 3;
+
+        System.out.println(quantidadeProdutos);
+    }
+}
