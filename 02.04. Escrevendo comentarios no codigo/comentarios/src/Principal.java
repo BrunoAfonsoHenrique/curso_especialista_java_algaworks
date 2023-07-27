73b1b8e048b3b59ca6c24d30409722842e55a100
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-07-27 15:05:12 -0300

    codigo-fonte da aula

---
+++ 02.04. Escrevendo comentarios no codigo/comentarios/src/Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        //Comentario de linha
+        System.out.println("Olá, mergulhador");
+
+        /*
+        Comentario de bloco
+         */
+    }
+}
