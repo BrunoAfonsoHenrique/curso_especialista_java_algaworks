3c5f282be56ffe5fc06b4ae3d1a77b165621eec2
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-07-28 13:20:01 -0300

    codigo-fonte da aula

---
+++ 02.17. Promocao aritmetica/promocao-aritmetica/src/PromocaoAritmetica2.java
@@ -0,0 +1,11 @@
+public class PromocaoAritmetica2 {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+        float y = 9.34f;
+        // long z = x * y; // não compila
+        float z = x * y;
+        System.out.println(z);
+    }
+}
