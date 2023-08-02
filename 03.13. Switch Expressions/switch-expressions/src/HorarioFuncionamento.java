c8816e44fb48f4aa3b834cd04eaacadb607ec522
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-02 11:05:33 -0300

    codigo-fonte da aula

--- 03.13. Switch Expressions/switch-expressions/src/HorarioFuncionamento.java
+++ 03.13. Switch Expressions/switch-expressions/src/HorarioFuncionamento.java
@@ -9,6 +9,9 @@ public class HorarioFuncionamento {
         String diaSemana = entrada.nextLine();
         //String horarioFuncionamento;
 
+        System.out.print("Mês (1 a 12): ");
+        int mes = entrada.nextInt();
+
         /*
         switch (diaSemana) {
             case "seg" -> horarioFuncionamento = "Fechado";
@@ -18,15 +21,20 @@ public class HorarioFuncionamento {
             case "sab", "dom" -> {
                 horarioFuncionamento = "08:00 às 12:00";
             }
-            default -> horarioFuncionamento = "Dia inválido";
+            default -> horarioFuncionamento = "Dia inválido"; // default não obrigatório
         }
         */
 
         String horarioFuncionamento = switch (diaSemana) {
-            case "seg" -> "Fechado";
+            case "seg" -> {
+                if (mes == 12){
+                    yield "08:00 às 16:00";
+                }
+                yield "Fechado";
+            }
             case "ter", "qua", "qui", "sex" -> "08:00 às 18:00";
             case "sab", "dom" -> "08:00 às 12:00";
-            default -> "Dia inválido";
+            default -> "Dia inválido"; // default obrigatório
         };
 
         System.out.printf("Horário de funcionamento: %s%n", horarioFuncionamento);
