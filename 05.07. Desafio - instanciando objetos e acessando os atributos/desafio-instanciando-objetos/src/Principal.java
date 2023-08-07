386477173a706e32571903a3243df74816f63ce1
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-07 09:48:35 -0300

    codigo-fonte da aula

---
+++ 05.07. Desafio - instanciando objetos e acessando os atributos/desafio-instanciando-objetos/src/Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Cachorro cachorro1 = new Cachorro("Tató", 6, "Vira-lata", "M");
+
+        Cachorro cachorro2 = new Cachorro("Belinha", 3, "Golden", "F");
+
+        System.out.println(cachorro1);
+        System.out.println("-----------");
+        System.out.println(cachorro2);
+    }
+}
