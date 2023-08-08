393b0a9561f73949df0a8f78a50118a2c691fad7
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-08 15:12:38 -0300

    codigo-fonte da aula

---
+++ 05.12. Inicializacao de objetos em variaveis de instancia/aula-poo/src/Carro.java
@@ -0,0 +1,16 @@
+public class Carro {
+
+    // Definição somente dos atributos de carro
+    String fabricante = "Forde";
+
+    String modelo;
+
+    String cor;
+
+    int anoFabricacao = 2021;
+
+    Pessoa proprietario = new Pessoa();
+
+    boolean desligado;
+
+}
