d022976b87977b548fa434a2a46c53e1cde758eb
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-10 12:49:21 -0300

    codigo-fonte da aula

---
+++ 05.28. Metodos que alteram o valor de parametro do tipo primitivo/precificacao/src/ServicoDePrecificacao.java
@@ -0,0 +1,8 @@
+public class ServicoDePrecificacao {
+
+    void calcularPrecoVenda(double precoCusto) {
+        // adiciona 20% de margem de lucro
+        precoCusto = precoCusto * 1.20;
+
+    }
+}
