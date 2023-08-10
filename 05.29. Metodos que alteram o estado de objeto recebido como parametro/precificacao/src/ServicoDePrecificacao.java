4cf743905aa8f50bbc2440c47eb84788a4c36aa0
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-10 12:51:40 -0300

    codigo-fonte da aula

---
+++ 05.29. Metodos que alteram o estado de objeto recebido como parametro/precificacao/src/ServicoDePrecificacao.java
@@ -0,0 +1,10 @@
+public class ServicoDePrecificacao {
+
+    void calcularPrecoVenda(double precoCusto) {
+        // adiciona 20% de margem de lucro
+        precoCusto = precoCusto * 1.20;
+
+        // Boa pratica: não alterar o valor do parametro
+
+    }
+}
