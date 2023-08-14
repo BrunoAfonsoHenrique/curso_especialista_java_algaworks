cfc715e0d0fe7abedeb907813d397238690f342e
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-14 14:43:24 -0300

    codigo-fonte da aula

---
+++ 07.01. Usando classes wrapper/wrappers-boxing/src/Principal1.java
@@ -0,0 +1,16 @@
+public class Principal1 {
+
+    public static void main(String[] args) {
+        // tipos primitivos
+        Integer diasParaEntrega;
+        Long codigoEntrega;
+        Float valorFrete;
+        Double valorTotal;
+        Character tipoCliente;
+        Boolean compraPaga;
+
+        // String é uma classe
+        String nomeCliente;
+    }
+
+}
