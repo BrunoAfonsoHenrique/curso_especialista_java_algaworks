3cc7a8410430337dd5dbbddf5d9901912d83ac7f
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-14 15:17:27 -0300

    codigo-fonte da aula

---
+++ 07.04. Comparando wrappers/wrappers-boxing/src/Principal1.java
@@ -0,0 +1,8 @@
+public class Principal1 {
+
+    public static void main(String[] args) {
+        Integer diasEntrega = 30;
+        // int diasEntregaInt = diasEntrega.intValue();
+        int diasEntregaInt = diasEntrega;
+    }
+}
---
+++ 07.04. Comparando wrappers/wrappers-boxing/src/Principal2.java
@@ -0,0 +1,9 @@
+public class Principal2 {
+
+    public static void main(String[] args) {
+        Integer numero1 = Integer.valueOf(127);
+        Integer numero2 = 127;
+
+        System.out.println(numero1.equals(numero2));
+    }
+}
