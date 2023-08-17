9231e068761b710cc4c97a9137ac4cfb2ef6bb4f
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-17 11:05:00 -0300

    codigo-fonte da aula

---
+++ 08.14. Boas praticas - retorne arrays ou colecoes vazias no lugar de null/cardapio/src/Cardapio.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cardapio {
+
+//    ItemCardapio[] itens = new ItemCardapio[0];
+    List<ItemCardapio> itens = new ArrayList<>();
+    void adicionarItem(ItemCardapio item) {
+        itens.add(item);
+
+//        itens = Arrays.copyOf(itens, itens.length + 1);
+//        itens[itens.length - 1] = item;
+    }
+
+    void removerItem(int indice) {
+        itens.remove(indice);
+//        ItemCardapio[] novosItens = new ItemCardapio[itens.length - 1];
+//
+//        System.arraycopy(itens, 0, novosItens, 0, indice);
+//        System.arraycopy(itens, indice + 1,
+//                novosItens, indice, novosItens.length - indice);
+//
+//        itens = novosItens;
+    }
+
+    void imprimirItensCardapio(double precoMinimo, double precoMaximo) {
+        for (ItemCardapio item : itens) {
+            if (item.possuiPrecoEntre(precoMinimo, precoMaximo)) {
+                item.imprimir();
+            }
+        }
+    }
+
+}
