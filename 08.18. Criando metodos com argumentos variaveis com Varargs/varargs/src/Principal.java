923dd65ebb35ed38715f3e5add9d269e54d00d67
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-17 14:27:11 -0300

    codigo-fonte da aula

---
+++ 08.18. Criando metodos com argumentos variaveis com Varargs/varargs/src/Principal.java
@@ -0,0 +1,24 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Fatura fatura = new Fatura();
+        fatura.numero = 123;
+        fatura.valorTotal = 1_293.55;
+
+        ServicoDeCobranca servicoDeCobranca = new ServicoDeCobranca();
+
+//        String[] emailCobranca = {"Joao@gmail.com", "maria@gmail.com"};
+//        servicoDeCobranca.pagar(fatura, emailCobranca);
+
+//        servicoDeCobranca.pagar(fatura,
+//                new String[]{"Joao@gmail.com", "maria@gmail.com"});
+
+//        servicoDeCobranca.pagar(fatura, new String[0]); // Array vazio
+
+//        servicoDeCobranca.pagar(fatura, new String[]{}); // mesma coisa que a linha acima
+
+        servicoDeCobranca.pagar(fatura, "Joao@gmail.com", "maria@gmail.com");
+
+        servicoDeCobranca.pagar(fatura);
+    }
+}
