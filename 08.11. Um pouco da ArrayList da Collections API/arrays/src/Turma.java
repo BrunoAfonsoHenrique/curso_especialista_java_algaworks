24d77e6ba5b353a6f02242908a9c18bd2a07bcc2
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-17 10:05:36 -0300

    codigo-fonte da aula

--- 08.11. Um pouco da ArrayList da Collections API/arrays/src/Turma.java
+++ 08.11. Um pouco da ArrayList da Collections API/arrays/src/Turma.java
@@ -1,23 +1,22 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Turma {
 
     String identificacao;
-
     String nomeProfessora;
-
-    Aluno[] alunos = new Aluno[0];
+    ArrayList<Aluno> alunos = new ArrayList<>();
 
     void adicionarAluno(Aluno aluno) {
-        this.alunos = Arrays.copyOf(this.alunos, this.alunos.length + 1);
-        alunos[alunos.length - 1] = aluno;
+        alunos.add(aluno);
+    }
+
+    void removerAluno(int indice) {
+        alunos.remove(indice);
     }
 
     void imprimirListaDeAlunos() {
         for (Aluno aluno : alunos) {
-            if (aluno != null) {
-                System.out.printf("%s (%d anos)%n", aluno.nome, aluno.idade);
-            }
+            System.out.printf("%s (%d anos)%n", aluno.nome, aluno.idade);
         }
     }
 
