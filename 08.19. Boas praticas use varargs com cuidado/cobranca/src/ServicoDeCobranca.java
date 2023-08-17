e69dfc5252c605dbe1158046d791fca764e7ae31
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-17 14:51:14 -0300

    codigo-fonte da aula

---
+++ 08.19. Boas praticas use varargs com cuidado/cobranca/src/ServicoDeCobranca.java
@@ -0,0 +1,26 @@
+import java.util.Objects;
+
+public class ServicoDeCobranca {
+
+    void pagar(Fatura fatura, String emailCobranca, String... emailsAdicionais) {
+
+        Objects.requireNonNull(fatura, "Informe a fatura");
+        Objects.requireNonNull(emailCobranca, "Informe o eamil cobrança");
+
+
+        System.out.printf("Fatura %d, no valor total de R$%.2f, foi paga!%n",
+                fatura.numero, fatura.valorTotal);
+
+        enviarNotificacao(fatura, emailCobranca);
+
+        for (String email : emailsAdicionais) {
+            enviarNotificacao(fatura, emailCobranca);
+        }
+
+    }
+
+    private void enviarNotificacao(Fatura fatura, String email) {
+        System.out.printf("Fatura %d enviada para %s%n", fatura.numero, email);
+    }
+
+}
