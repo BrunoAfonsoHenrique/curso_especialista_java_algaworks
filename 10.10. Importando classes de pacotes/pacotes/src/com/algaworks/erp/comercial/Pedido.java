1058545a9fc5e305f83cc8dba3ddf3906cc76ac7
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-18 14:39:09 -0300

    codigo-fonte da aula

---
+++ 10.10. Importando classes de pacotes/pacotes/src/com/algaworks/erp/comercial/Pedido.java
@@ -0,0 +1,13 @@
+package com.algaworks.erp.comercial;
+
+import com.algaworks.erp.estoque.Produto;
+
+import java.util.List;
+
+public class Pedido {
+
+    List<Produto> produtos;
+
+    Cliente cliente;
+
+}
