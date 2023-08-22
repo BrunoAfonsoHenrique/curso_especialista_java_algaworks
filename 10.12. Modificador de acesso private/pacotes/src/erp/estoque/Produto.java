07d49495f7a649e231333b5258d469ca6f0866ce
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-22 09:31:32 -0300

    codigo-fonte da aula

--- 10.12. Modificador de acesso private/pacotes/src/erp/estoque/Produto.java
+++ 10.12. Modificador de acesso private/pacotes/src/erp/estoque/Produto.java
@@ -7,28 +7,32 @@ public class Produto {
 
     static final int QUANTIDADE_ESTOQUE_INCIAL = 100;
 
-    final String codigo;
-    public String nome;
-    public int quantidadeEstoque;
+    private final String codigo;
+    private String nome;
+    private int quantidadeEstoque;
 
     public Produto() {
         this("Sem nome");
 
     }
 
-    public Produto(String nome) {
+    private Produto(String nome) {
         this(nome, QUANTIDADE_ESTOQUE_INCIAL);
 
     }
 
-    public Produto(String nome, int estoqueInicial) {
+    private Produto(String nome, int estoqueInicial) {
         Objects.requireNonNull(nome, "Nome é obrigatório");
         if (estoqueInicial < 0) {
             throw new IllegalArgumentException("Estoque inicial não pode ser negativo");
         }
         this.nome = nome;
         this.quantidadeEstoque = estoqueInicial;
-        this.codigo = UUID.randomUUID().toString(); // Gera um código aleatório
+        this.codigo = gerarCodigo(); // Gera um código aleatório (String)
+    }
+
+    private String gerarCodigo() {
+        return UUID.randomUUID().toString(); // Gera um código aleatório (String)
     }
 
 }
