fef8dc90ee72918fb1938a523e4253cb5c66bc7d
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-23 12:14:32 -0300

    codigo-fonte da aula

---
+++ 11.05. Desafio - encapsulamento e JavaBeans/desafio-encapsulamento/src/com/algaworks/model/Fornecedor.java
@@ -0,0 +1,23 @@
+package com.algaworks.model;
+
+public class Fornecedor {
+
+    private String nome;
+
+    public Fornecedor() {
+
+    }
+
+    public Fornecedor(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
