363f0f39ee690ffb59a67081c0d654637ff4c0e4
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-08-31 10:08:34 -0300

    codigo-fonte da aula

---
+++ 12.12. Criando construtores com parametros na superclasse e subclasses/banco/src/com/algaworks/banco/ContaInvestimento.java
@@ -0,0 +1,25 @@
+package com.algaworks.banco;
+
+public class ContaInvestimento extends Conta {
+
+    private double valorTotalRendimentos;
+
+    public ContaInvestimento() {
+        super(0);
+    }
+
+    public ContaInvestimento(Titular titular, int agencia, int numero) {
+        super(titular, agencia, numero);
+    }
+
+    public double getValorTotalRendimentos() {
+        return valorTotalRendimentos;
+    }
+
+    public void creditarRendimentos(double percentualJuros) {
+        double valorRendimentos = getSaldo() * percentualJuros / 100;
+        this.valorTotalRendimentos += valorRendimentos;
+        depositar(valorRendimentos);
+    }
+
+}
