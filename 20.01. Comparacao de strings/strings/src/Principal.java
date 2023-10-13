fb051b0fcb69e5c6cc05dde447d433df12e41cb5
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-10-12 21:39:36 -0300

    codigo-fonte da aula

---
+++ 20.01. Comparacao de strings/strings/src/Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+
+    public static void main(String[] args) {
+
+        String nome1 = "Joao";
+        String nome2 = new String("Joao");
+
+        // System.out.println(nome1 == nome2);
+
+        System.out.println(nome1.equals(nome2));
+
+    }
+}
