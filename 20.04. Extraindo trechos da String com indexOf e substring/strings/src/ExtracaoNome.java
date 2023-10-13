7b195be48da81750add9eb90bea7198b57551425
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-10-12 22:28:34 -0300

    codigo-fonte da aula

---
+++ 20.04. Extraindo trechos da String com indexOf e substring/strings/src/ExtracaoNome.java
@@ -0,0 +1,32 @@
+public class ExtracaoNome {
+
+    public static void main(String[] args) {
+
+        String nome = "Joao Silva Souza";
+
+//        System.out.println(nome.indexOf(" ")); // 4
+//        System.out.println(nome.substring(5));
+//
+//        System.out.println(nome.substring(5,10));
+
+//        System.out.println(obterSobrenome(nome));
+        System.out.println(obterSegundoNome(nome));
+    }
+
+    private static String obterSobrenome(String nome) {
+        int posicaoPrimeiroEspaco = nome.indexOf(" ");
+        if (posicaoPrimeiroEspaco < 0) {
+            throw new RuntimeException("Nao é um nome completo");
+        }
+        return nome.substring(posicaoPrimeiroEspaco + 1);
+    }
+
+    private static String obterSegundoNome(String nome) {
+        int posicaoPrimeiroEspaco = nome.indexOf(" ");
+        int posicaoSegundoEspaco = nome.indexOf(" ", posicaoPrimeiroEspaco + 1);
+        if (posicaoPrimeiroEspaco < 0) {
+            throw new RuntimeException("Nao é um nome completo");
+        }
+        return nome.substring(posicaoPrimeiroEspaco + 1, posicaoSegundoEspaco);
+    }
+}
