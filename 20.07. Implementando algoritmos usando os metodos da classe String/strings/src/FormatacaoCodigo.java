9767b36953edba44ad5f830454bc3b46cd6bb1a7
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-10-17 20:28:28 -0300

    codigo-fonte da aula

---
+++ 20.07. Implementando algoritmos usando os metodos da classe String/strings/src/FormatacaoCodigo.java
@@ -0,0 +1,15 @@
+public class FormatacaoCodigo {
+
+    public static void main(String[] args) {
+
+        int codigo = 123;
+        String codigoFormatado = preencherEsquerda(String.valueOf(codigo) , '0', 10);
+
+        System.out.println(codigoFormatado);
+    }
+
+    public static String preencherEsquerda(String texto, char caracter, int tamanhoTotal) {
+
+        return String.valueOf(caracter).repeat(tamanhoTotal - texto.length()) + texto;
+    }
+}
