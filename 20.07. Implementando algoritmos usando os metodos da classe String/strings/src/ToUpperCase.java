7895144b60b83305585ac5068f7aaf697158607b
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-10-17 20:03:22 -0300

    codigo-fonte da aula

---
+++ 20.07. Implementando algoritmos usando os metodos da classe String/strings/src/ToUpperCase.java
@@ -0,0 +1,12 @@
+public class ToUpperCase {
+
+    public static void main(String[] args) {
+
+        String nome = "Bruno";
+
+        String nomeMaiusculo = nome.toUpperCase(); // retorna uma nova String em caixa alta
+
+        System.out.println(nome);
+        System.out.println(nomeMaiusculo);
+    }
+}
