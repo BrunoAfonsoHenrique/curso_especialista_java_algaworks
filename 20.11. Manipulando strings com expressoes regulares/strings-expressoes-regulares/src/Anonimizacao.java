73be992334274fe4a814c60533d3a583a338e5bf
Bruno Afonso <bruno_afonso_henrique@outlook.com>
2023-11-02 15:23:45 -0300

    codigo-fonte da aula

---
+++ 20.11. Manipulando strings com expressoes regulares/strings-expressoes-regulares/src/Anonimizacao.java
@@ -0,0 +1,10 @@
+public class Anonimizacao {
+
+    public static void main(String[] args) {
+        String html = "<a href=\"mailto:joao@gmail.com\">\n   joao@gmail.com  </a>\n" +
+                "<a>\n   abc@algaworks.com</a><a>xyz@algaworks.com</a>" +
+                "<strong>maria@algaworks.com</strong>";
+        
+    }
+
+}
